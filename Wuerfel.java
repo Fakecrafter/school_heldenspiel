cb86d13c44dff72a339db8d6427beb8bf85264f1
Zaid Fakecrafter <fakecrafter@protonmail.com>
2023-08-23 20:18:18 +0200

    changed behaviour of Wuerfel

--- Wuerfel.java
+++ Wuerfel.java
@@ -1,46 +1,28 @@
-/**
- * Wuerfel 
- * 
- * @author GK EF (Abi 2025)
- * @version 15.08.2023
- */
-
 public class Wuerfel
 {
 
-    // Attribute der Klasse Wuerfel
     private int augenzahl;
     private int seitenzahl;
 
     /**
      * Konstruktor fuer Objekte der Klasse TWuerfel
      */
-    public Wuerfel(int pSeitenzahl)    
-    {        
+    public Wuerfel(int pSeitenzahl)
+    {
         // Ein Wuerfel muss mindestens 2 Seiten besietzen
         if(pSeitenzahl < 2) pSeitenzahl = 2;
         seitenzahl = pSeitenzahl;
+	augenzahl = 0;
         werfen();
     }
-
-    // Anfang Methoden
-    /**
-     * Der Wuerfel wird einmal geworfen und seine 
-     * Augenzahl damit geaendert
-     */
-    public void werfen() {
-        // ermittle eine Zufallszahl zwischen 1 und der Seitenzahl 
-        // und setze diese in das Attribut augenzahl
-        //
-        // Die Methode random aus der Klasse Math        
-        // erzeugt eine Zufallszahl
-        // zwischen 0 und 1 (rationale Zahl)
+    public int werfen() {
+	// Math.random() generiert Zahl zwischen 0 und 1
+	// auf gewuenschte Range skalieren (indem man mit seitenzahl multipliziert)
         augenzahl =  (int) ((Math.random()*seitenzahl)+1);
-    }
-    
-    public int gibAugenzahl()
-    {
-        return augenzahl;
+	return augenzahl;
     }
 
+    public int gibAugenzahl() {
+	return augenzahl;
+    }
 }
